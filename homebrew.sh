ccaaf5157cfa4c7fde1c63a79250fb34541e0d7c
Danny Smith <hi@danny.is>
2016-10-27 12:33:20 +0100

    Bugfixes

--- homebrew.sh
+++ homebrew.sh
@@ -8,13 +8,12 @@ fi
 HOMEBREW_PREFIX="/usr/local"
 
 if [ -d "$HOMEBREW_PREFIX" ]; then
-  if ! [ -r "$HOMEBREW_PREFIX" ]; then
-    echo $passwd | sudo -S -k chown -R "$LOGNAME:admin" /usr/local
-  fi
+  green_echo "Chowning $HOMEBREW_PREFIX to \"$LOGNAME:admin\"..."
+  echo $passwd | sudo -S chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
 else
   echo $passwd | sudo -S -k mkdir "$HOMEBREW_PREFIX"
   echo $passwd | sudo -S -k chflags norestricted "$HOMEBREW_PREFIX"
-  echo $passwd | sudo -S -k chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
+  echo $passwd | sudo -S chown -R "$LOGNAME:admin" "$HOMEBREW_PREFIX"
 fi
 
 if ! command -v brew >/dev/null; then
@@ -33,18 +32,25 @@ if brew list | grep -Fq brew-cask; then
 fi
 
 # Update Homebrew
+fancy_echo "Running brew update..."
 brew update
+green_echo "done."
 
+fancy_echo "Tapping Brewfile and installing bundle..."
 # Tap bre-bundle
 brew tap Homebrew/bundle
 
 # Install everything in Brewfile
 brew bundle
+green_echo "done."
 
 # Clear Up
+fancy_echo "Cleaning Up..."
 brew prune
 brew cleanup
 brew cask cleanup
 brew doctor
+brew cask doctor
+green_echo "done."
 
 green_echo "Done with homebrew installs!"
