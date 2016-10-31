0fcf10c9fb935c873c5e9518d8416d6981377170
Danny Smith <hi@danny.is>
2016-10-31 11:27:31 +0000

    Added --no-document flag to .gemrc

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,5 +7,6 @@ curl -O https://raw.githubusercontent.com/spartaglobal/laptop/master/assets/dotf
 curl -O https://raw.githubusercontent.com/spartaglobal/laptop/master/assets/dotfiles/.irbrc
 curl -O https://raw.githubusercontent.com/spartaglobal/laptop/master/assets/dotfiles/.gitconfig
 curl -O https://raw.githubusercontent.com/spartaglobal/laptop/master/assets/dotfiles/.git_status_in_prompt.sh
+echo "gem: --no-document" > $HOME/.gemrc
 cd $LAPTOP_INSTALL_DIR
 fancy_echo "dotfiles done!"
