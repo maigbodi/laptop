3c57b40c4ca1dc22c4962ae17fb4900edd6c2fcc
Tech-A51 <tech-a51@Tech-A51s-MacBook-Air.local>
2017-09-21 16:40:47 +0100

    fixed oh-my-zsh config to include .zshrc file

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -7,12 +7,11 @@ fancy_echo "Downloading custom zsh theme..."
 
 cat sparta.zsh-theme > "$HOME/.oh-my-zsh/themes/sparta.zsh-theme"
 
-fancy_echo "Editing .zshrc..."
-
-cat .zshrc > "$HOME/.zshrc"
 
 fancy_echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+fancy_echo "Editing .zshrc..."
 
+cat .zshrc > "$HOME/.zshrc"
 
