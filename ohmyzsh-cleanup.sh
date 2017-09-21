f03085d9949553d4a58a26281d6392c2529adb68
Ollie Holden <odholden@gmail.com>
2017-09-21 17:24:41 +0100

    fixed filepaths for extra ohmyzsh config

--- ohmyzsh-cleanup.sh
+++ ohmyzsh-cleanup.sh
@@ -1,7 +1,7 @@
 # Updating .zshrc with necessary rbenv setup and path variables
 fancy_echo "Editing .zshrc..."
-cat .zshrc > "$HOME/.zshrc"
+(curl -s https://raw.githubusercontent.com/spartaglobal/laptop/master/.zshrc) > "$HOME/.zshrc"
 
 # Installing custom oh-my-zsh theme
 fancy_echo "Downloading custom zsh theme..."
-cat sparta.zsh-theme > "$HOME/.oh-my-zsh/themes/sparta.zsh-theme"
+(curl -s https://raw.githubusercontent.com/spartaglobal/laptop/master/sparta.zsh-theme) > "$HOME/.oh-my-zsh/themes/sparta.zsh-theme"
