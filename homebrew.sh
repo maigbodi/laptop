9d7ceba08c9999661ab48c6654dc3749f89f80ad
Ollie Holden <odholden@gmail.com>
2017-11-27 12:19:14 +0000

    reverted back to old homebrew install

--- homebrew.sh
+++ homebrew.sh
@@ -1,6 +1,6 @@
 # Install Homebrew
 fancy_echo "Installing Homebrew..."
-mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Update Homebrew
 fancy_echo "Running brew update..."
